dc5cc7c261164de1e633e5a54d18666a9539491f
Erica Pitts <ericavp77@gmail.com>
2017-11-06 15:27:14 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,13 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+
+function addElementToBeginningOfArray(array = [], "") {
+  var new1 = ["", ...array]
+      return new1
+}
+
+function destructivelyAddElementToBeginningOfArray(){
+  var x = []
+    var x = x.unshift
+      return x
+  }
