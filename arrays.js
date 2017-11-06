037945a9d16b0227e6ca7bf5a2b764093281171b
Erica Pitts <ericavp77@gmail.com>
2017-11-06 16:49:17 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -4,7 +4,7 @@ function addElementToBeginningOfArray(array,  element) {
    var y = [element, ...array]
         return y
     }
-    
+
     function destructivelyAddElementToBeginningOfArray(array,  element) {
        array.unshift(element)
             return array
