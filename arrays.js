de514bfcf43bc1e5fa324206fe07164e78b1f9d3
Erica Pitts <ericavp77@gmail.com>
2017-11-06 17:11:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -25,5 +25,6 @@ function addElementToBeginningOfArray(array,  element) {
                     }
 
                     function destructivelyRemoveElementFromBeginningOfArray(array) {
-                      return array.shift()
-                        }
+                     array.shift()
+                        return array
+                          }
