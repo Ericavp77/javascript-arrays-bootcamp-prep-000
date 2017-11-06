50f1dd7da3e094b5b62cda817957535e6c0eacb4
Erica Pitts <ericavp77@gmail.com>
2017-11-06 17:15:28 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -28,3 +28,8 @@ function addElementToBeginningOfArray(array,  element) {
                      array.shift()
                         return array
                           }
+                          
+                          function removeElementFromBeginningOfArray(array) {
+                            array.slice(1)
+                            return array
+                              }
